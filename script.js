7c0bbdfeb53ab1b45ac94b19b8d509925eee847f
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-20 18:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+let i=shortHex.length-3,temp="#",isAnyCapital=false;
 const extendHex = (shortHex) => {
-  // write your code here
-};
+  while (i<shortHex.length) {
+  	temp+=shortHex[i];
+	  i++;
+	  if(shortHex[i]>=65&&shortHex[i]<=90){
+		  isAnyCapital=true;
+	  }
+  }
 
+	if(isAnyCapital){
+		return temp.toUpperCase();
+	}
+	return temp;
+};
+const shortHex;
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+ shortHex= prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
